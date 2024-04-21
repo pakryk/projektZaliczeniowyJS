4633fd3b8a7206b43d8ba1181358b689bd4c8229
pakryk <patryk.szczepanski1992@gmail.com>
2024-04-21 20:52:53 +0200

    DOM Elements added in script.js file

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,48 @@
 // Selecting DOM elements
+// Zmienne dla klas HTML
+const header = document.querySelector('.header');
+const headerTitle = document.querySelector('.header__title');
+const headerInput = document.querySelector('.header__input');
+
+const main = document.querySelector('.main');
+const listBike = document.querySelector('.list__bike');
+const bike = document.querySelector('.bike');
+const bikeImg = document.querySelector('.bike__img');
+const bikeDescription = document.querySelector('.bike__description');
+const bikeName = document.querySelector('.bike__description--name');
+const bikePrice = document.querySelector('.bike__description--price');
+
+const selectedBikeContainer = document.querySelector('.selected-bike__container');
+const selectedBikeName = document.querySelector('.selected-bike__name');
+const selectedBikePrice = document.querySelector('.selected-bike__price');
+
+const accessoriesContainer = document.querySelector('.accessories-container');
+const accessoriesTitle = document.querySelector('.accessories__title');
+const accessoriesSport = document.querySelector('.accessories__sport');
+const accessoriesStreet = document.querySelector('.accessories__street');
+const accessoriesTravel = document.querySelector('.accessories__travel');
+const accessoriesName = document.querySelectorAll('.accessories__name');
+const accessoriesList = document.querySelectorAll('.accessories__list');
+const accessoriesItem = document.querySelectorAll('.accessories__item');
+const accessoriesPrice = document.querySelectorAll('.accessories__price');
+
+const buyerTitle = document.querySelector('.buyer-title');
+const form = document.querySelector('.form');
+const nameInput = document.querySelector('#name');
+const destinationInput = document.querySelector('#destination');
+const dateInput = document.querySelector('#date');
+const pricing = document.querySelector('.pricing');
+const leasingRadio = document.querySelector('#leasing');
+const cashRadio = document.querySelector('#cash');
+
+const backButton = document.querySelector('.btn__back');
+const buyButton = document.querySelector('.btn__buy');
+
+const finalSection = document.querySelector('.section-final');
+const finalBikeImg = document.querySelector('.bike__img');
+const thankingText = document.querySelector('.thanking-text');
+const deliveryText = document.querySelector('.delivery-txt');
+const mainPageButton = document.querySelector('.btn__main-page');
 
 
 // Bike data
